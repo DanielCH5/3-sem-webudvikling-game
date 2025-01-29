540fe6bc4147b4ddc9c74d82d789d931a4ec0146
DanielCH5 <danielc.hansen1@gmail.com>
2025-01-29 01:02:33 +0100

    Kommentarer

--- movement.js
+++ movement.js
@@ -1,11 +1,14 @@
+//Player constant
 const player = document.getElementById("player");
 
-window.addEventListener("keydown", function (event)  {
-    if (event.key === "w" || event.key === "W"){
+
+//Event listener der lytter til hvornår W,A,S,D er trykket ned og retter sig efter det ift. top position og left position i HTML
+window.addEventListener("keydown", function (event) {
+    if (event.key === "w" || event.key === "W") {
         let currentTop = parseInt(window.getComputedStyle(player).top);
         player.style.top = currentTop - 3 + "px";
     }
-    if (event.key === "a" || event.key === "A"){
+    if (event.key === "a" || event.key === "A") {
         let currentLeft = parseInt(window.getComputedStyle(player).left);
         player.style.left = currentLeft - 3 + "px";
     }
@@ -13,11 +16,10 @@ window.addEventListener("keydown", function (event)  {
         let currentTop = parseInt(window.getComputedStyle(player).top);
         player.style.top = currentTop + 3 + "px";
     }
-    if (event.key === "d" || event.key === "D"){
+    if (event.key === "d" || event.key === "D") {
         let currentLeft = parseInt(window.getComputedStyle(player).left);
         player.style.left = currentLeft + 3 + "px";
     }
-
 
 
 })
